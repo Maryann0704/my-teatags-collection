9181dbc70d1e0c31161dda1839c46b18885d4cba
Narushevich Marianna <marianna.naru@gmail.com>
2019-11-27 21:35:21 +0300

    after reset database add closing for session

--- src/main/java/commands/CmdResetDataBase.java
+++ src/main/java/commands/CmdResetDataBase.java
@@ -3,6 +3,7 @@ package commands;
 import dao.Dao;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 public class CmdResetDataBase extends Cmd{
@@ -10,6 +11,8 @@ public class CmdResetDataBase extends Cmd{
     public Cmd execute(HttpServletRequest req) throws SQLException {
         Dao dao = Dao.getDao();
         dao.resetDataBase();
+        HttpSession session = req.getSession();
+        session.invalidate();
         return Actions.INDEX.command;
     }
 }
